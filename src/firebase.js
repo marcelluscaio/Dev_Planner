1270f970119ea32b57b6c81328906ab11dd62da4
Alexandre <alexmcs@gmail.com>
2023-02-02 17:34:33 -0300

    Configurando variáveis de ambiente

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: process.env.DEV_PLANNER_FIREBASE_APIKEY,
-    authDomain: process.env.DEV_PLANNER_FIREBASE_DOMAIN,
-    projectId: process.env.DEV_PLANNER_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.DEV_PLANNER_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.DEV_PLANNER_FIREBASE_SENDER_ID,
-    appId: process.env.DEV_PLANNER_FIREBASE_APP_ID,
-    measurementId: process.env.DEV_PLANNER_FIREBASE_MEASUREMENT_ID
+    apiKey: process.env.REACT_APP_FB_APIKEY,
+    authDomain: process.env.REACT_APP_FB_DOMAIN,
+    projectId: process.env.REACT_APP_FB_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FB_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FB_SENDER_ID,
+    appId: process.env.REACT_APP_FB_APP_ID,
+    measurementId: process.env.REACT_APP_FB_MEASUREMENT_ID
 };
 
 // Initialize Firebase
@@ -22,12 +22,3 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
-
-
-    // apiKey: process.env.DEV_PLANNER_FIREBASE_APIKEY,
-    // authDomain: process.env.DEV_PLANNER_FIREBASE_DOMAIN,
-    // projectId: process.env.DEV_PLANNER_FIREBASE_PROJECT_ID,
-    // storageBucket: process.env.DEV_PLANNER_FIREBASE_STORAGE_BUCKET,
-    // messagingSenderId: process.env.DEV_PLANNER_FIREBASE_SENDER_ID,
-    // appId: process.env.DEV_PLANNER_FIREBASE_APP_ID,
-    // measurementId: process.env.DEV_PLANNER_FIREBASE_MEASUREMENT_ID
