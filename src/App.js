fd8f02bb0ea3266770d6fda15e561d74ddafe541
Alexandre <alexmcs@gmail.com>
2023-02-08 21:38:14 -0300

    refatorando app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
 import SignIn from './components/auth/SignIn';
 import SignUp from './components/auth/SignUp';
-import AuthDetails from './components/AuthDetails';
 
 function App() {
   return (
     <div className="App">
       <SignIn />
       <SignUp />
-      <AuthDetails />
     </div>
   );
 }
